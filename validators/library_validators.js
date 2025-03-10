010166991f6504dd2741efceb83536cf6a154d4f
Akweleyp <patriciaakweleyn@gmail.com>
2025-03-10 16:30:31 +0000

    routes,models and controllers

---
+++ validators/library_validators.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+
+export const addLibraryValidator = Joi.object({
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    pages: Joi.string().required(),
+    publishedDate: Joi.date().required(),
+    genre: Joi.string().required(),
+    language: Joi.string().required(),
+    publisher: Joi.string().required(),
+    summary: Joi.string().required(),
+
+})
