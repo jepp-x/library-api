5c6e66279d9e3b0b88eca14256d6842e8c9a1b6b
Jeffrey Nana Boakye <jaeyblaq2131@gmail.com>
2025-03-11 18:29:11 -0400

    search books

--- routes/library_routes.js
+++ routes/library_routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { addBook, deleteBook, getBook, updateBook } from "../controllers/library_controllers.js";
+import { addBook, deleteBook, getBook, searchBooks, updateBook } from "../controllers/library_controllers.js";
 
 //Create product router
 const libraryRouter = Router();
@@ -9,9 +9,10 @@ const libraryRouter = Router();
 libraryRouter.post('/books', addBook);
 
 libraryRouter.get('/books', getBook);
+libraryRouter.get('/books/search', searchBooks); //I added it here so the code looks organised
 
 libraryRouter.patch('/books/:id', updateBook);
-libraryRouter.delete('/books/id', deleteBook);
+libraryRouter.delete('/books/:id', deleteBook);
 
 //Export router
 export default libraryRouter; 
