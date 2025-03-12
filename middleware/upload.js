abd8dd0f8235dee947a76c542415e45e7134822e
asantex <agyei22@mail.com>
2025-03-12 14:49:33 +0000

    modelanduploadchanges

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,9 @@
 import multer from "multer";
-import{multerSaveFilesorg} from "multer-savefilesorg";
-
+import { multerSaveFilesOrg } from "multer-savefilesorg";
 
 
 export const remoteUpload = multer({
-    storage:multerSaveFilesorg({
+    storage:multerSaveFilesOrg({
         apisAccessToken:process.env.SAVEFILESORG_API_KEY,
         relativePath:'/library-api/*'
     })
